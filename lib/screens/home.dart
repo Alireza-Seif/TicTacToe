75c781c45407249dbae38aa9800816be679762b9
alirezaseif20 <alirezaariye@gmail.com>
2024-04-13 22:03:18 +0330

    feat: add getScoreBorad & getGridView

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,16 +8,79 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('TicTacToe'),
+        title: const Text(
+          'TicTacToe',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.grey[900],
       ),
-      body: const SafeArea(
-        child: Column(
-          children: [],
+      body: getGridView(),
+    );
+  }
+
+  GridView getGridView() {
+    return GridView.builder(
+      itemCount: 9,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget getScoreBorad() {
+    return const Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  '0',
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  '0',
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
